260958fa8f8a9a0fb43decb0238fa9f8c5142bf1
Quintanilla270 <30916119+Quintanilla270@users.noreply.github.com>
2023-10-17 00:00:21 -0500

    xhr functinality

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,11 +1,17 @@
 
 function getDataFromForm() {
-  const form = document.getElementById('ajaxCall');
-  const f_name = form.elements['fname'];
-  const l_name = form.elements['lname'];
+  var xhr = new XMLHttpRequest;
+  xhr.open('GET', 'index.html', true);
+  xhr.send('search=arduino');
+  xhr.onload = function() {
+    if(xhr.status === 200) {
+      alert('LOADED')
+    }
+  }
   runAjax(f_name, l_name);
   alert("it worked!");
 }
 
 function runAjax(first_name, last_name) {
+  $.
 }
