18e688db3de9bb718cd6f1c693e32e0e26f0e3b8
Quintanilla270 <30916119+Quintanilla270@users.noreply.github.com>
2023-10-16 22:25:22 -0500

    js changes

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,4 +1,11 @@
 
 function getDataFromForm() {
+  const form = document.getElementById('ajaxCall');
+  const f_name = form.elements['fname'];
+  const l_name = form.elements['lname'];
+  runAjax(f_name, l_name);
   alert("it worked!");
+}
+
+function runAjax(first_name, last_name) {
 }
