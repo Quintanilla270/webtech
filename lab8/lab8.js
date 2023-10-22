982f1473d96fa7c9efe537ceaed29f506f7a80d4
Quintanilla270 <30916119+Quintanilla270@users.noreply.github.com>
2023-10-21 19:30:37 -0500

    corrections to runAjax function

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,17 +1,16 @@
 
 function getDataFromForm() {
-  var xhr = new XMLHttpRequest;
-  xhr.open('GET', 'index.html', true);
-  xhr.send('search=arduino');
-  xhr.onload = function() {
-    if(xhr.status === 200) {
-      alert('LOADED')
-    }
-  }
   runAjax(f_name, l_name);
   alert("it worked!");
 }
 
 function runAjax(first_name, last_name) {
-  
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', './ajax.php?fname=${fname}&lame=${lname}', true);
+  xhr.onload = function() {
+    if(xhr.status === 200) {
+      alert('LOADED')
+    }
+  }
+  xhr.send();
 }
