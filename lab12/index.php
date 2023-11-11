00660689c82e45e5925ae302348934692dad3497
Quintanilla270 <30916119+Quintanilla270@users.noreply.github.com>
2023-11-11 12:38:41 -0600

    update cookie

--- lab12/index.php
+++ lab12/index.php
@@ -4,13 +4,14 @@ $cookie_name = "cs4413";
 $cookie_value = "cs4413";
 $expiration_time = time() + 60 * 60 * 24 * 7;
 
-setcookie($cookie_name, $cookie_value, $expiration_time);
-
-if(!isset($_COOKIE[$cookie_name])) {
-    echo "Cookie named '" . $cookie_name . "' is not set!";
+if(isset($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is already set!";
+    echo "Value is: " . $_COOKIE[$cookie_name];
 }
 else {
-    echo "Cookie '" . $cookie_name . "' is set!<br>";
+    echo "Cookie '" . $cookie_name . "' is not set!<br>";
+    echo "Setting cookie now..."
+    setcookie($cookie_name, $cookie_value, $expiration_time);
     echo "Value is: " . $_COOKIE[$cookie_name];
 }
 
