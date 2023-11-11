26a11df9b65737baec371eaaa34df05d7bb281ec
Quintanilla270 <30916119+Quintanilla270@users.noreply.github.com>
2023-11-11 12:43:59 -0600

    ;

--- lab12/index.php
+++ lab12/index.php
@@ -10,7 +10,7 @@ if(isset($_COOKIE[$cookie_name])) {
 }
 else {
     echo "Cookie '" . $cookie_name . "' is not set!<br>";
-    echo "Setting cookie now..."
+    echo "Setting cookie now...";
     setcookie($cookie_name, $cookie_value, $expiration_time);
     echo "Value is: " . $_COOKIE[$cookie_name];
 }
